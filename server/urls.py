592a368706fc11ab3dc45835172a414219450dc3
Anas Hagras <anas.hagras1999@gmail.com>
2024-04-06 02:52:30 +0200

    added-DRF-users

--- server/urls.py
+++ server/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/v1", include("users.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("api/v1/", include("users.urls"))]
 
 # add static and media to urls
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import viewsets
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class TestRender(APIView):
+    def get(self, request):
+        return render(request, "templates/test.html")
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
